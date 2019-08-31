b6ca3671a4645ca807926904b10808d00ce413ff
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-08-31 16:46:53 +0200

    Завершение lesson01

--- script/script.js
+++ script/script.js
@@ -1,2 +1,12 @@
 'use stript';
 
+let money, 
+    income, 
+    addExpenses, 
+    deposit, 
+    mission, 
+    period;
+
+alert('Говорят, что тяжело в учениях...');
+
+console.log('Посмотрим :)');
