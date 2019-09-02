548f19cf15faf2d28970fb31da6953a1d4f9ecb3
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-02 15:28:20 +0200

    выполнение 4)

--- script/script_05.js
+++ script/script_05.js
@@ -2,99 +2,94 @@
 
 let money,                                    
 inCome = 'фриланс', 
-//addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'еда, проезд, комуналка'),
-//deposit = confirm('Есть ли у вас депозит в банке?'), 
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'еда, проезд, комуналка'),
+deposit = confirm('Есть ли у вас депозит в банке?'), 
 mission = 123456, 
-period;
+period,
+exprenses1,
+exprenses2;
 
-
-
-
-
+// 1) Переписать функцию start циклом do while
 let start = function() {
-	 money = +prompt('Ваш месячный доход? ', '5432');
-	  
-	 while(isNaN(money) || money === '' || money === null){
-		money = +prompt('Ваш месячный доход? ', '5432');
-		console.log('Ваш месячный доход? ', money); 
-	 }
-};
-start();
+	 do{ 
+		money = prompt('Ваш месячный доход? ', 5432);
+	 }while(isNaN(money) || money === '' || money === null || money < -money);
+	};
+ start();
+console.log('Ваш месячный доход? ', money);
+ 
 
+// Вывод типов данных
+// let showTypeof = function(data) {
+// 	console.log('Основные данные: ', data, typeof(data));
+// };
+// showTypeof(money);
+// showTypeof(inCome);
+// showTypeof(deposit);
 
-
-
-
-let showTypeof = function(data) {
-	console.log('Основные данные: ', data, typeof(data));
-};
-showTypeof(money);
-showTypeof(inCome);
-//showTypeof(deposit);
-
-//console.log(addExpenses.split(', '));
-
- let exprenses1,
-	 exprenses2,
-	 
-     budgetMonth = money - (exprenses1 + exprenses1);
-
-
-
-
-
+// Вывод массива addExpenses
+// console.log(addExpenses.split(', '));
 
  
+
+// 2) Добавить валидацию (проверку) на данные которые мы получаем на вопрос 'Во сколько это обойдется?’  
 let getExpensesMonth =function() {
-	let sum = 0;
+	let sum = 0,
+	    checkSum;
+
 	for(let i = 0; i < 2; i++){
        if(i === 0){
 		exprenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Люблю потусоваться, знаете...');
 	   }
-
 	   if(i === 1){
 		exprenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Вечер пятницы...');
 	   }
+		do{ 
+			checkSum = +prompt('Во сколько это обойдется?', 555);
+		 }while(isNaN(checkSum) || checkSum === 0 || checkSum === null);
+		 sum += checkSum; 
+		}
+		return sum;
+	};
+getExpensesMonth();
+let expensesAmount = getExpensesMonth();
+console.log('Чистый месячный доход: ', expensesAmount);
 
-		sum += +prompt('Во сколько это обойдется?', 555);
-	}
-	return sum;
-};
-let expensesAmount = getExpensesMonth()
-console.log('sum: :', expensesAmount);
 
 
-
-
-
-
-// 1) — функция getAccumulatedMonth возвращает Накопления за месяц (Доходы минус расходы)
+// 3) Если getTargetMonth возвращает нам отрицательное значение то вместо “Цель будет достигнута”, необходимо выводить “Цель не будет достигнута”
 let getAccumulatedMonth = function(){
-	return money - (exprenses1 + exprenses1);
+	let cleanMoney;
+	cleanMoney = money - expensesAmount;
+	if(cleanMoney > 0) {
+		return ('Цель будет достигнута!');
+	} else if(cleanMoney < -cleanMoney){
+		return ('Цель не будет достигнута!');
+	}
 };
 getAccumulatedMonth();
 // Результат сохранить в переменную accumulatedMonth
 let accumulatedMonth = getAccumulatedMonth();
+console.log('Чистый доход: ', accumulatedMonth);
 
 // 1) — функция getTargetMonth подсчитывает за какой период будет достигнута цель, зная результат месячного накопления 
 let getTargetMonth = function() {
-	return  mission / accumulatedMonth; 
+	return  mission / expensesAmount;
 };
 getTargetMonth();
 period = getTargetMonth();
-console.log('Сумму в:', mission, ' - Вы накопите за ', period, 'месяцев');
+console.log('Период накопления: ', period, 'месяцев');
+console.log('Сумму: ', mission, ' - Вы накопите за ', period, 'месяцев');
 console.log('А точнее за', Math.floor(period));
 
 
-// console.log('Сумму в:', mission, ' - Вы накопите за ', period, 'месяцев');
-// console.log('А точнее за', Math.ceil(period));
 
-let budgetDay = money / 30;
-console.log('Даход за день: ', budgetDay);
-console.log('А точнее: ', Math.floor(budgetDay));
+let budgetDay = expensesAmount / 30;
+//console.log('Даход за день: ', budgetDay);
+// console.log('А точнее: ', Math.floor(budgetDay));
 
 
-// 2) — Оставить функцию getStatusIncome, которые написали в уроке
+
 let getStatusIncome = function(){
 	if(budgetDay > 800) {
 		return ('Высокий уровень дохода');
