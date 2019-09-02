93ff09762602f03ecae5d47b3a9760b049a02fbc
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-02 16:22:33 +0200

    завергение lesson05

--- script/script_05.js
+++ script/script_05.js
@@ -4,20 +4,21 @@ let money,
 inCome = 'фриланс', 
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'еда, проезд, комуналка'),
 deposit = confirm('Есть ли у вас депозит в банке?'), 
-mission = 123456, 
+mission = 123456,
+budgetDay, 
 period,
 exprenses1,
 exprenses2;
 
 // 1) Переписать функцию start циклом do while
 let start = function() {
-	 do{ 
+	do{ 
 		money = prompt('Ваш месячный доход? ', 5432);
-	 }while(isNaN(money) || money === '' || money === null || money < -money);
-	};
- start();
-console.log('Ваш месячный доход? ', money);
- 
+	}while(isNaN(money) || money === '' || money === null || money < -money);
+};
+start();
+console.log('Ваш месячный доход: ', money);
+
 
 // Вывод типов данных
 // let showTypeof = function(data) {
@@ -30,37 +31,40 @@ console.log('Ваш месячный доход? ', money);
 // Вывод массива addExpenses
 // console.log(addExpenses.split(', '));
 
- 
-
 // 2) Добавить валидацию (проверку) на данные которые мы получаем на вопрос 'Во сколько это обойдется?’  
 let getExpensesMonth =function() {
 	let sum = 0,
-	    checkSum;
+	checkSum;
 
 	for(let i = 0; i < 2; i++){
-       if(i === 0){
-		exprenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Люблю потусоваться, знаете...');
-	   }
-	   if(i === 1){
-		exprenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Вечер пятницы...');
-	   }
+		if(i === 0){
+			exprenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Люблю потусоваться, знаете...');
+		}
+		if(i === 1){
+			exprenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Вечер пятницы...');
+		}
 		do{ 
 			checkSum = +prompt('Во сколько это обойдется?', 555);
-		 }while(isNaN(checkSum) || checkSum === 0 || checkSum === null);
-		 sum += checkSum; 
-		}
-		return sum;
-	};
+		}while(isNaN(checkSum) || checkSum === 0 || checkSum === null);
+		sum += checkSum; 
+	}
+	return sum;
+};
 getExpensesMonth();
 let expensesAmount = getExpensesMonth();
-console.log('Чистый месячный доход: ', expensesAmount);
+console.log('Обязательные ежемесячные расходы: ', expensesAmount);
 
+
+let getCleanMoney = function() {
+	return money - expensesAmount;
+};
+getCleanMoney();
+let cleanMoney = getCleanMoney();
+console.log(' Выш чистый доход: ', cleanMoney);
 
 
 // 3) Если getTargetMonth возвращает нам отрицательное значение то вместо “Цель будет достигнута”, необходимо выводить “Цель не будет достигнута”
 let getAccumulatedMonth = function(){
-	let cleanMoney;
-	cleanMoney = money - expensesAmount;
 	if(cleanMoney > 0) {
 		return ('Цель будет достигнута!');
 	} else if(cleanMoney < -cleanMoney){
@@ -70,27 +74,21 @@ let getAccumulatedMonth = function(){
 getAccumulatedMonth();
 // Результат сохранить в переменную accumulatedMonth
 let accumulatedMonth = getAccumulatedMonth();
-console.log('Чистый доход: ', accumulatedMonth);
+console.log('Статус вашгего дохода: ', accumulatedMonth);
 
-// 1) — функция getTargetMonth подсчитывает за какой период будет достигнута цель, зная результат месячного накопления 
-let getTargetMonth = function() {
-	return  mission / expensesAmount;
-};
-getTargetMonth();
-period = getTargetMonth();
-console.log('Период накопления: ', period, 'месяцев');
-console.log('Сумму: ', mission, ' - Вы накопите за ', period, 'месяцев');
-console.log('А точнее за', Math.floor(period));
+// функция getTargetMonth подсчитывает за какой период будет достигнута цель, зная результат месячного накопления 
+// let getTargetMonth = function() {
+// 	return  mission / expensesAmount;
+// };
+// getTargetMonth();
+// period = getTargetMonth();
+// console.log('Период накопления: ', period, 'месяцев');
+// console.log('Сумму ', mission, ' - Вы накопите за ', period, 'месяцев');
+// console.log('А точнее за', Math.floor(period));
 
-
-
-let budgetDay = expensesAmount / 30;
-//console.log('Даход за день: ', budgetDay);
-// console.log('А точнее: ', Math.floor(budgetDay));
-
-
-
+// 4) Если budgetDay отрицательное значение то вместо уровня дохода пусть выводится сообщение “Что то пошло не так”
 let getStatusIncome = function(){
+	budgetDay = cleanMoney / 30;  
 	if(budgetDay > 800) {
 		return ('Высокий уровень дохода');
 	} else if(budgetDay > 300 && budgetDay < 800) {
@@ -104,4 +102,6 @@ let getStatusIncome = function(){
 	}
 };
 getStatusIncome();
-console.log(getStatusIncome());
+let statusBudgetDay = getStatusIncome();
+console.log('Ваш дневной доход: ', budgetDay);
+console.log('Статус дневного дохода: ', statusBudgetDay);
