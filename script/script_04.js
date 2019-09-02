cbc4f9120e37694c0d43b83fd9f1e29e1f21042c
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-02 19:33:31 +0200

    последнее сохранение lesson04

--- script/script_04.js
+++ script/script_04.js
@@ -9,7 +9,7 @@ period;
 
 // 1) — Оставить функцию showTypeof, которые написали в уроке
 let showTypeof = function(data) {
-    console.log('Основные данные: ', data, typeof(data));
+	console.log('Основные данные: ', data, typeof(data));
 };
 showTypeof(money);
 showTypeof(inCome);
@@ -20,19 +20,18 @@ showTypeof(deposit);
 let exprensesMonth1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Люблю потусоваться, знаете...'),
 exprenses1 = +prompt('Во сколько это обойдется?', 987),
 exprensesMonth2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Вечер пятницы...'),
-exprenses2 = +prompt('Во сколько это обойдется?', 567),
-budgetMonth = money - (exprenses1 + exprenses1);
-
+exprenses2 = +prompt('Во сколько это обойдется?', 567);
 
 // 1) getExpensesMonth, которая возвращает сумму всех расходов за месяц
 let getExpensesMonth =function() {
-return exprensesMonth2 + exprenses2;
+	return exprenses1 + exprenses2;
 };
 getExpensesMonth();
+let expensesMonth = getExpensesMonth();
 
 // 1) — функция getAccumulatedMonth возвращает Накопления за месяц (Доходы минус расходы)
 let getAccumulatedMonth = function(){
-    return money - (exprenses1 + exprenses1);
+	return money - expensesMonth;
 };
 getAccumulatedMonth();
 // Результат сохранить в переменную accumulatedMonth
@@ -40,35 +39,39 @@ let accumulatedMonth = getAccumulatedMonth();
 
 // 1) — функция getTargetMonth подсчитывает за какой период будет достигнута цель, зная результат месячного накопления 
 let getTargetMonth = function() {
-    return  mission / accumulatedMonth; 
+	return  mission / accumulatedMonth; 
 };
 getTargetMonth();
 period = getTargetMonth();
 console.log('Сумму в:', mission, ' - Вы накопите за ', period, 'месяцев');
 console.log('А точнее за', Math.floor(period));
 
- 
+
 // console.log('Сумму в:', mission, ' - Вы накопите за ', period, 'месяцев');
 // console.log('А точнее за', Math.ceil(period));
 
-let budgetDay = money / 30;
+let getBudgetDay = function() {
+	return accumulatedMonth / 30;
+};
+getBudgetDay();
+let budgetDay = getBudgetDay();
 console.log('Даход за день: ', budgetDay);
-console.log('А точнее: ', Math.floor(budgetDay));
+console.log('А точнее: ', Math.ceil(budgetDay));
 
 
 // 2) — Оставить функцию getStatusIncome, которые написали в уроке
 let getStatusIncome = function(){
-if(budgetDay > 800) {
- return ('Высокий уровень дохода');
-} else if(budgetDay > 300 && budgetDay < 800) {
-    return ('Средний уровень дохода');
-} else if(budgetDay > 0 && budgetDay < 300) {
-    return ('Низкий уровень дохода');
-}  else if (budgetDay < -budgetDay) {
-    return ('Что-то пошло не так');
-} else {
-    return ('Что-то пошло не так');
-}
+	if(budgetDay > 800) {
+		return ('Высокий уровень дохода');
+	} else if(budgetDay > 300 && budgetDay < 800) {
+		return ('Средний уровень дохода');
+	} else if(budgetDay > 0 && budgetDay < 300) {
+		return ('Низкий уровень дохода');
+	}  else if (budgetDay < -budgetDay) {
+		return ('Что-то пошло не так');
+	} else {
+		return ('Что-то пошло не так');
+	}
 };
 getStatusIncome();
 console.log(getStatusIncome());
