57460cee6caaa3758d35cdab4a876c739202f6fe
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-03 18:47:19 +0200

    конечка lesson04

--- script/script_04.js
+++ script/script_04.js
@@ -15,8 +15,6 @@ showTypeof(money);
 showTypeof(inCome);
 showTypeof(deposit);
 
-//console.log(addExpenses.split(', '));
-
 let exprensesMonth1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Люблю потусоваться, знаете...'),
 exprenses1 = +prompt('Во сколько это обойдется?', 987),
 exprensesMonth2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ' Вечер пятницы...'),
@@ -26,46 +24,37 @@ exprenses2 = +prompt('Во сколько это обойдется?', 567);
 let getExpensesMonth =function() {
 	return exprenses1 + exprenses2;
 };
-getExpensesMonth();
 let expensesMonth = getExpensesMonth();
+console.log('Cумма всех расходов за месяц: ' + expensesMonth);
 
 // 1) — функция getAccumulatedMonth возвращает Накопления за месяц (Доходы минус расходы)
 let getAccumulatedMonth = function(){
 	return money - expensesMonth;
 };
-getAccumulatedMonth();
 // Результат сохранить в переменную accumulatedMonth
 let accumulatedMonth = getAccumulatedMonth();
+console.log('Накопления за месяц: ' + accumulatedMonth);
 
 // 1) — функция getTargetMonth подсчитывает за какой период будет достигнута цель, зная результат месячного накопления 
 let getTargetMonth = function() {
 	return  mission / accumulatedMonth; 
 };
-getTargetMonth();
 period = getTargetMonth();
-console.log('Сумму в:', mission, ' - Вы накопите за ', period, 'месяцев');
-console.log('А точнее за', Math.floor(period));
-
-
-// console.log('Сумму в:', mission, ' - Вы накопите за ', period, 'месяцев');
-// console.log('А точнее за', Math.ceil(period));
-
+console.log('Сумму в: ' + mission + ' - Вы накопите за ' + Math.floor(period) + ' месяцев');
+ 
 let getBudgetDay = function() {
 	return accumulatedMonth / 30;
 };
-getBudgetDay();
 let budgetDay = getBudgetDay();
-console.log('Даход за день: ', budgetDay);
-console.log('А точнее: ', Math.ceil(budgetDay));
-
+console.log('Даход за день: ' + Math.ceil(budgetDay));
 
 // 2) — Оставить функцию getStatusIncome, которые написали в уроке
 let getStatusIncome = function(){
 	if(budgetDay > 800) {
 		return ('Высокий уровень дохода');
-	} else if(budgetDay > 300 && budgetDay < 800) {
+	} else if(budgetDay >= 300 && budgetDay < 800) {
 		return ('Средний уровень дохода');
-	} else if(budgetDay > 0 && budgetDay < 300) {
+	} else if(budgetDay >= 0 && budgetDay < 300) {
 		return ('Низкий уровень дохода');
 	}  else if (budgetDay < -budgetDay) {
 		return ('Что-то пошло не так');
