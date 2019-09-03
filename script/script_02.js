cd613cad1fb240a961ad33d4699333762d4c68ee
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-03 14:32:02 +0200

    поправки lesson02

--- script/script_02.js
+++ script/script_02.js
@@ -9,18 +9,21 @@ mission = 100000,
 period = 12;
 
 // 2) -Вывести в консоль тип данных значений переменных money, income, deposit
-console.log(typeof money);                  
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(money, '-тип данных значения money: ', typeof money);                  
+console.log(income, '-тип данных значения income: ', typeof income);
+console.log(deposit, '-тип данных значения deposit: ', typeof deposit);
+
+// 2) - Вывести в консоль длину строки income
+console.log('Длина строки income: ', income.length);
 
 // 2) -Вывести в консоль “Период (period) месяцев” и “Цель заработать (mission) долларов
 console.log('Цель заработать', mission, 'долларов'); 
 console.log('Период', period, 'месяцев');
 
 // 2) -Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
-console.log(addExpenses.toLowerCase().split(','));
+console.log(addExpenses.toLowerCase().split(', '));
 
-// 2)-Объявить переменную budgetDay и присвоить дневной бюджет(доход за месяц / 30), вывести в консоль и остаток от деления
+// 2)-Объявить переменную budgetDay и присвоить дневной бюджет(доход за месяц / 30), вывести в консоль и остаток 
 let budgetDay = money / 30;                                        //дневной бюджет
 console.log('Дневной бюджет: ', budgetDay);
-console.log('Остаток от деления дневного бюджета: ', (budgetDay % 30));   //остаток от деления
+console.log('Остаток от деления дневного бюджета: ', (money % 30));   //остаток от деления
