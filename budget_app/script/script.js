bf5488f9d33bea216cd68ca0235afa2c36041e65
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-09 23:07:12 +0200

    переделал

--- budget_app/script/script.js
+++ budget_app/script/script.js
@@ -1,88 +1,94 @@
 'use srtict';
 
-let start = function(){
-    let btnCalculate = document.getElementById('start');
+let btnCalculate = document.getElementById('start'),
+    reset = document.getElementById('censel'),
+    btnPlus = document.getElementsByTagName('button'),
+    incomePlus = btnPlus[0],
+    expensesPlus = btnPlus[1],
+    depositCheck = document.querySelectorAll('#deposit-check'),
+    inputAddIncome = document.querySelectorAll('.additional_income-item'),
+    incomeItemFirst = inputAddIncome[0],
+    incomeItemSecond = inputAddIncome[1],
+    
+    budgetMonthValue = document.querySelectorAll('.budget_month-value'),
+    budgetDayValue = document.querySelectorAll('.budget_day-value'),
+    expensesMonthValue = document.querySelectorAll('.expenses_month-value'),
+    additionalIncomeValue = document.querySelectorAll('.additional_income-value'),
+    additionalExpensesValue = document.querySelectorAll('.additional_expenses-value'),
+    incomePeriodValue = document.querySelectorAll('.income_period-value'),
+    targetMonthValue = document.querySelectorAll('.target_month-value'),
+  
+    salaryAmount = document.querySelectorAll('.salary-amount'),
+    incomeTitle = document.querySelectorAll('.income-title'),
+    incomeAmount = document.querySelectorAll('.income-amount'),
+    expensesTitle = document.querySelectorAll('.expenses-title'),
+    expensesAmount = document.querySelectorAll('.expenses-amount'),
+    additionalExpensesItem = document.querySelectorAll('.additional_expenses-item'),
+    depositmount = document.querySelectorAll('.deposit-amount'),
+    depositPercent = document.querySelectorAll('.deposit-percent'),
+    periodSelect = document.querySelectorAll('.period-select');
 
-        console.log(btnCalculate);
-        return btnCalculate; 
-};
+console.log(btnCalculate, reset, btnPlus, expensesPlus, depositCheck, inputAddIncome, incomeItemFirst, 
+            incomeItemSecond, budgetMonthValue, budgetDayValue, expensesMonthValue, additionalIncomeValue, 
+            additionalExpensesValue, incomePeriodValue, targetMonthValue, salaryAmount, incomeTitle, incomeAmount, 
+            expensesTitle, expensesAmount, additionalExpensesItem, depositmount, depositPercent, periodSelect);
+
+// let start = function(){
+//     let btnCalculate = document.getElementById('start');
+//         return btnCalculate; 
+// };
 
 
- let getBtnPlus = function(){
-     let btnPlus = document.getElementsByTagName('button'),
-         btnPlusFirst = btnPlus[0],
-         btnPlusSecond = btnPlus[1];
-
-         console.log(btnPlusFirst);
-         console.log(btnPlusSecond);
-
- };
+//  let getBtnPlus = function(){
+//      let btnPlus = document.getElementsByTagName('button'),
+//          btnPlusFirst = btnPlus[0],
+//          btnPlusSecond = btnPlus[1];
+//  };
  
 
-let getCheckbox = function(){
-    let checkbox = document.querySelectorAll('#deposit-check');
-      console.log(checkbox);
-    return checkbox;  
-};
+// let getCheckbox = function(){
+//     let checkbox = document.querySelectorAll('#deposit-check');
+      
+//     return checkbox;  
+// };
 
 
-let getInputAddIncome = function(){
-    let inputAddIncome = document.querySelectorAll('.additional_income-item'),
-        incomeItemFirst = inputAddIncome[0],
-        incomeItemSecond = inputAddIncome[1];
-
-        console.log(incomeItemFirst);
-        console.log(incomeItemSecond);
-};
+// let getInputAddIncome = function(){
+//     let inputAddIncome = document.querySelectorAll('.additional_income-item'),
+//         incomeItemFirst = inputAddIncome[0],
+//         incomeItemSecond = inputAddIncome[1];
+// };
 
 
-let getAllBlock = function(){
-    let budgetMonthValue = document.querySelectorAll('.budget_month-value'),
-        budgetDayValue = document.querySelectorAll('.budget_day-value'),
-        expensesMonthValue = document.querySelectorAll('.expenses_month-value'),
-        additionalIncomeValue = document.querySelectorAll('.additional_income-value'),
-        additionalExpensesValue = document.querySelectorAll('.additional_expenses-value'),
-        incomePeriodValue = document.querySelectorAll('.income_period-value'),
-        targetMonthValue = document.querySelectorAll('.target_month-value');
-        
-        console.log(budgetMonthValue);
-        console.log(budgetDayValue);
-        console.log(expensesMonthValue);
-        console.log(additionalIncomeValue);
-        console.log( additionalExpensesValue);
-        console.log(incomePeriodValue);
-        console.log(targetMonthValue);
-};
+// let getAllBlock = function(){
+//     let budgetMonthValue = document.querySelectorAll('.budget_month-value'),
+//         budgetDayValue = document.querySelectorAll('.budget_day-value'),
+//         expensesMonthValue = document.querySelectorAll('.expenses_month-value'),
+//         additionalIncomeValue = document.querySelectorAll('.additional_income-value'),
+//         additionalExpensesValue = document.querySelectorAll('.additional_expenses-value'),
+//         incomePeriodValue = document.querySelectorAll('.income_period-value'),
+//         targetMonthValue = document.querySelectorAll('.target_month-value');
+// };
 
 
-let getRemainBlock = function(){
-    let  salaryAmount = document.querySelectorAll('.salary-amount'),
-         incomeTitle = document.querySelectorAll('.income-title'),
-         incomeAmount = document.querySelectorAll('.income-amount'),
-         expensesTitle = document.querySelectorAll('.expenses-title'),
-         expensesAmount = document.querySelectorAll('.expenses-amount'),
-         additionalExpensesItem = document.querySelectorAll('.additional_expenses-item'),
-         depositmount = document.querySelectorAll('.deposit-amount'),
-         depositPercent = document.querySelectorAll('.deposit-percent'),
-         periodSelect = document.querySelectorAll('.period-select');
+// let getRemainBlock = function(){
+//     let  salaryAmount = document.querySelectorAll('.salary-amount'),
+//          incomeTitle = document.querySelectorAll('.income-title'),
+//          incomeAmount = document.querySelectorAll('.income-amount'),
+//          expensesTitle = document.querySelectorAll('.expenses-title'),
+//          expensesAmount = document.querySelectorAll('.expenses-amount'),
+//          additionalExpensesItem = document.querySelectorAll('.additional_expenses-item'),
+//          depositmount = document.querySelectorAll('.deposit-amount'),
+//          depositPercent = document.querySelectorAll('.deposit-percent'),
+//          periodSelect = document.querySelectorAll('.period-select');
+// };
 
-
-         console.log(salaryAmount);
-         console.log(incomeTitle);
-         console.log(incomeAmount);
-         console.log(expensesTitle);
-         console.log(expensesAmount);
-         console.log(additionalExpensesItem);
-         console.log(depositPercent);
-         console.log(periodSelect);
-};
-
-start();
-getBtnPlus();
-getCheckbox();
-getInputAddIncome();
-getAllBlock();
-getRemainBlock();
+// start();
+// getBtnPlus();
+// getCheckbox();
+// getInputAddIncome();
+// getAllBlock();
+// getRemainBlock();
 
 
 
--- you-dont-know-js/script/script.js
+++ you-dont-know-js/script/script.js
@@ -1,23 +1,46 @@
 'use strict';
 
+
 let books = document.querySelectorAll('.books'),
     book = document.querySelectorAll('.book'),
     body = document.querySelector('body'),  
     heading = document.querySelectorAll('h2'),
-    headingLink = document.querySelectorAll('a'); 
+    headingLink = document.querySelectorAll('a'), 
+    callRestoreChapter = document.querySelectorAll('ul'),
+    callRestoreChapterElem = document.querySelectorAll('li');
+
+console.log(book);
+console.log(callRestoreChapter);
+console.log(callRestoreChapterElem);
 
 // выравнял главы по порядку
-let getReplaceBook = function(){   
+let getReplaceBooks = function(){   
         books[0].insertBefore(book[1], book[0]);
         books[0].insertBefore(book[2], null);
         books[0].insertBefore(book[4], book[3]);
-   };
+};
+
+
+let getReplaceBook2 = function(){ 
+    callRestoreChapter[0].insertBefore(callRestoreChapterElem[5], null);  
+    callRestoreChapter[0].insertBefore(callRestoreChapterElem[4], callRestoreChapterElem[8]);
+    callRestoreChapter[0].insertBefore(callRestoreChapterElem[8], callRestoreChapterElem[6]);
+    callRestoreChapter[0].insertBefore(callRestoreChapterElem[6], callRestoreChapterElem[8]);
+};
+
+
+let getReplaceBook5 = function(){ 
+    callRestoreChapter[5].insertBefore(callRestoreChapterElem[53], null);  
+    callRestoreChapter[5].insertBefore(callRestoreChapterElem[55], callRestoreChapterElem[48]);
+    callRestoreChapter[5].insertBefore(callRestoreChapterElem[55], callRestoreChapterElem[49]);
+    callRestoreChapter[5].insertBefore(callRestoreChapterElem[48], callRestoreChapterElem[52]);
+};
 
 
 // замена картинки
 let getReplaceImg = function(){
      body.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)');
-   };
+};
 
 
 // замена названия главы
@@ -26,30 +49,28 @@ let getReplaceImg = function(){
          newHeadingLink.textContent = '"Книга 3. this и Прототипы Объектов"';
          heading[4].removeChild(headingLink[4]);
          heading[4].appendChild(newHeadingLink);
- };
+};
  
 
  // удаление рекламы
  let getDeleteReclam = function(){
      let reclam = document.querySelectorAll('.adv');
          body.removeChild(reclam[0]);    
- };
+};
  
 
  // востановление глав
- let callRestoreChapter = document.querySelectorAll('ul'),
-     callRestoreChapterElem = document.querySelectorAll('li');
  let getRestoreChapter = function(){
     callRestoreChapter[1].insertBefore(callRestoreChapterElem[10], null);
     callRestoreChapter[1].insertBefore(callRestoreChapterElem[11], null);
- };
+};
  
 
  // изменение в 6-й главе
  let getRestoreChapterNew = function(){
     let  book = document.querySelectorAll('ul'),
-         charper = document.querySelectorAll('li');
-    let  clone  = charper[56].cloneNode(true);
+         charper = document.querySelectorAll('li'),
+         clone  = charper[56].cloneNode(true);
          clone.textContent = 'Глава 8: За пределами ES6” и поставить её в правильное место';
          book[5].appendChild(clone);
          book[5].insertBefore(charper[55], clone); 
@@ -57,7 +78,9 @@ let getReplaceImg = function(){
 }; 
 
 
-getReplaceBook();
+getReplaceBooks();
+getReplaceBook2();
+getReplaceBook5();
 getReplaceImg();
 getHeading();
 getDeleteReclam();
