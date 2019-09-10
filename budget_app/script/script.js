50869df5091dab25b6e558ca87733e176b3ea613
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-10 09:40:11 +0200

    исправил querySelector, expenses-title и target-amount

--- budget_app/script/script.js
+++ budget_app/script/script.js
@@ -1,94 +1,44 @@
 'use srtict';
 
 let btnCalculate = document.getElementById('start'),
-    reset = document.getElementById('censel'),
+    cansel = document.getElementById('cansel'),
     btnPlus = document.getElementsByTagName('button'),
     incomePlus = btnPlus[0],
     expensesPlus = btnPlus[1],
-    depositCheck = document.querySelectorAll('#deposit-check'),
+    depositCheck = document.querySelector('#deposit-check'),
     inputAddIncome = document.querySelectorAll('.additional_income-item'),
     incomeItemFirst = inputAddIncome[0],
     incomeItemSecond = inputAddIncome[1],
     
-    budgetMonthValue = document.querySelectorAll('.budget_month-value'),
-    budgetDayValue = document.querySelectorAll('.budget_day-value'),
-    expensesMonthValue = document.querySelectorAll('.expenses_month-value'),
-    additionalIncomeValue = document.querySelectorAll('.additional_income-value'),
-    additionalExpensesValue = document.querySelectorAll('.additional_expenses-value'),
-    incomePeriodValue = document.querySelectorAll('.income_period-value'),
-    targetMonthValue = document.querySelectorAll('.target_month-value'),
-  
-    salaryAmount = document.querySelectorAll('.salary-amount'),
-    incomeTitle = document.querySelectorAll('.income-title'),
-    incomeAmount = document.querySelectorAll('.income-amount'),
-    expensesTitle = document.querySelectorAll('.expenses-title'),
-    expensesAmount = document.querySelectorAll('.expenses-amount'),
-    additionalExpensesItem = document.querySelectorAll('.additional_expenses-item'),
-    depositmount = document.querySelectorAll('.deposit-amount'),
-    depositPercent = document.querySelectorAll('.deposit-percent'),
-    periodSelect = document.querySelectorAll('.period-select');
+    budgetMonthValue = document.querySelector('.budget_month-value'),
+    budgetDayValue = document.querySelector('.budget_day-value'),
+    expensesMonthValue = document.querySelector('.expenses_month-value'),
+    additionalIncomeValue = document.querySelector('.additional_income-value'),
+    additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+    incomePeriodValue = document.querySelector('.income_period-value'),
+    targetMonthValue = document.querySelector('.target_month-value'),
+    
+    salaryAmount = document.querySelector('.salary-amount'),
+    incomeTitle = document.querySelector('.income-title'),
+    incomeAmount = document.querySelector('.income-amount'),
+    // expenses-title
+    expensesTitle = document.querySelectorAll('.expenses-title')[1],
+    expensesAmount = document.querySelector('.expenses-amount'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    // target-amount
+    targetAmount = document.querySelector('.target-amount'),
 
-console.log(btnCalculate, reset, btnPlus, expensesPlus, depositCheck, inputAddIncome, incomeItemFirst, 
+    depositAmount = document.querySelector('.deposit-amount'),
+    depositPercent = document.querySelector('.deposit-percent'),
+    periodSelect = document.querySelector('.period-select');
+
+console.log(btnCalculate, cansel, btnPlus, expensesPlus, depositCheck, inputAddIncome, incomeItemFirst, 
             incomeItemSecond, budgetMonthValue, budgetDayValue, expensesMonthValue, additionalIncomeValue, 
             additionalExpensesValue, incomePeriodValue, targetMonthValue, salaryAmount, incomeTitle, incomeAmount, 
-            expensesTitle, expensesAmount, additionalExpensesItem, depositmount, depositPercent, periodSelect);
-
-// let start = function(){
-//     let btnCalculate = document.getElementById('start');
-//         return btnCalculate; 
-// };
-
-
-//  let getBtnPlus = function(){
-//      let btnPlus = document.getElementsByTagName('button'),
-//          btnPlusFirst = btnPlus[0],
-//          btnPlusSecond = btnPlus[1];
-//  };
+            expensesTitle, expensesAmount, additionalExpensesItem, targetAmount, depositAmount, depositPercent, periodSelect);
  
 
-// let getCheckbox = function(){
-//     let checkbox = document.querySelectorAll('#deposit-check');
-      
-//     return checkbox;  
-// };
-
-
-// let getInputAddIncome = function(){
-//     let inputAddIncome = document.querySelectorAll('.additional_income-item'),
-//         incomeItemFirst = inputAddIncome[0],
-//         incomeItemSecond = inputAddIncome[1];
-// };
-
-
-// let getAllBlock = function(){
-//     let budgetMonthValue = document.querySelectorAll('.budget_month-value'),
-//         budgetDayValue = document.querySelectorAll('.budget_day-value'),
-//         expensesMonthValue = document.querySelectorAll('.expenses_month-value'),
-//         additionalIncomeValue = document.querySelectorAll('.additional_income-value'),
-//         additionalExpensesValue = document.querySelectorAll('.additional_expenses-value'),
-//         incomePeriodValue = document.querySelectorAll('.income_period-value'),
-//         targetMonthValue = document.querySelectorAll('.target_month-value');
-// };
-
-
-// let getRemainBlock = function(){
-//     let  salaryAmount = document.querySelectorAll('.salary-amount'),
-//          incomeTitle = document.querySelectorAll('.income-title'),
-//          incomeAmount = document.querySelectorAll('.income-amount'),
-//          expensesTitle = document.querySelectorAll('.expenses-title'),
-//          expensesAmount = document.querySelectorAll('.expenses-amount'),
-//          additionalExpensesItem = document.querySelectorAll('.additional_expenses-item'),
-//          depositmount = document.querySelectorAll('.deposit-amount'),
-//          depositPercent = document.querySelectorAll('.deposit-percent'),
-//          periodSelect = document.querySelectorAll('.period-select');
-// };
-
-// start();
-// getBtnPlus();
-// getCheckbox();
-// getInputAddIncome();
-// getAllBlock();
-// getRemainBlock();
+            console.log(document.querySelectorAll('.expenses-title'));
 
 
 
