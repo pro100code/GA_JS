20790fa268557787aef0613d107c134b3ecbec79
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2019-09-23 01:20:37 +0200

    последние изминения lesson12 task6

--- lesson12/task6/script.js
+++ lesson12/task6/script.js
@@ -1,36 +1,42 @@
-function Time(deadline){
+function Time(deadline) {
     let goodDay = document.querySelector('.goodDay'),
-     goodDatSpan = goodDay.childNodes[3],
-     toDay = document.querySelector('.toDay'),
-     toDaySpan = toDay.childNodes[3],
-     curTime = document.querySelector('.curTime'),
-     curTimeSpan = curTime.childNodes[3],
-     endYear = document.querySelector('.endYear'),
-    endYearSpan = endYear.childNodes[3],
-    dayArr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-   
-    function getTimeRemaining(){
+        goodDatSpan = goodDay.childNodes[3],
+        toDay = document.querySelector('.toDay'),
+        toDaySpan = toDay.childNodes[3],
+        curTime = document.querySelector('.curTime'),
+        curTimeSpan = curTime.childNodes[3],
+        endYear = document.querySelector('.endYear'),
+        endYearSpan = endYear.childNodes[3],
+        dayArr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+    function getTimeRemaining() {
         let date = new Date(),
-         day = date.getDay() - 1,
-         localTime = date.toLocaleTimeString('en'),
-         localTimeRU = date.toLocaleTimeString('ru'),
-         dateStop = new Date(deadline).getTime(),
-         dateNow = date.getTime(),
-         timeRemaining = (dateStop - dateNow) / 1000,
-         stopDay = Math.floor(timeRemaining / 60 / 60 / 24);
-    
-        return {timeRemaining, day, localTime, stopDay, localTimeRU};
-    } 
+            day = date.getDay() - 1,
+            localTime = date.toLocaleTimeString('en'),
+            localTimeRU = date.toLocaleTimeString('ru'),
+            dateStop = new Date(deadline).getTime(),
+            dateNow = date.getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            stopDay = Math.floor(timeRemaining / 60 / 60 / 24);
+
+        return {
+            timeRemaining,
+            day,
+            localTime,
+            stopDay,
+            localTimeRU
+        };
+    }
 
     function CalculateDate() {
         let timer = getTimeRemaining();
         // Показывает день неледели
-        for (let i = 0; i < dayArr.length; i++){
-                if (i === timer.day){
-                    toDaySpan.textContent = dayArr[i];
-                }
+        for (let i = 0; i < dayArr.length; i++) {
+            if (i === timer.day) {
+                toDaySpan.textContent = dayArr[i];
+            }
         }
-        
+
         // показывает текущую дату
         curTimeSpan.textContent = timer.localTime;
 
@@ -40,14 +46,14 @@ function Time(deadline){
         // время суток
         if (timer.localTimeRU > 4 || timer.localTimeRU < 12) {
             goodDatSpan.textContent = 'Утро';
-        } else if (timer.localTimeRU > 12|| timer.localTimeRU < 17) {
+        } else if (timer.localTimeRU > 12 || timer.localTimeRU < 17) {
             goodDatSpan.textContent = 'день';
-        } else if (timer.localTimeRU > 17 + 'AM'|| timer.localTimeRU < 23) {
+        } else if (timer.localTimeRU > 17 + 'AM' || timer.localTimeRU < 23) {
             goodDatSpan.textContent = 'вечер';
         } else {
             goodDatSpan.textContent = 'ночь';
         }
-        
+
 
         if (timer.timeRemaining > 0) {
             setTimeout(CalculateDate, 1000);
@@ -55,8 +61,5 @@ function Time(deadline){
 
     }
     CalculateDate();
-
-    
 }
-
 Time('1 january 2020');
